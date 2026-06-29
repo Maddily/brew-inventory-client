48e3943550e4fc8c6e05b6c07f862b2d4b631be1
Maddily <115709272+Maddily@users.noreply.github.com>
2026-06-29 16:38:11 +0300

    Create BottomNav component

---
+++ src/components/BottomNav/BottomNav.jsx
@@ -0,0 +1,19 @@
+import { mdiDotsGrid, mdiPackageVariantClosed, mdiPlus } from "@mdi/js";
+import BottomNavLink from "../BottomNavLink/BottomNavLink";
+import styles from "./BottomNav.module.css";
+
+function BottomNav() {
+  return (
+    <nav className={styles["bottom-nav"]} aria-label="Bottom navigation">
+      <BottomNavLink value="Categories" path="/" iconPath={mdiDotsGrid} />
+      <BottomNavLink
+        value="All products"
+        path="/products"
+        iconPath={mdiPackageVariantClosed}
+      />
+      <BottomNavLink value="Add" path="/products/new" iconPath={mdiPlus} />
+    </nav>
+  );
+}
+
+export default BottomNav;
