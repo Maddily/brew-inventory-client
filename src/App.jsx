f5a5392f3751bf6e0794e691e44c496747fac3a4
Maddily <115709272+Maddily@users.noreply.github.com>
2026-06-29 16:38:52 +0300

    Render primary and bottom nav bars

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import NavBar from "./components/NavBar/NavBar";
+import BottomNav from "./components/BottomNav/BottomNav";
+import "./styles/normalize.css";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <BottomNav />
+    </>
+  );
+}
+
+export default App;
