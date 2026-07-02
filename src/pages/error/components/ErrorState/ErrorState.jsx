e0b05aa1f532f2a2641ae8c39d1f33bc787e300d
Maddily <115709272+Maddily@users.noreply.github.com>
2026-07-02 16:42:13 +0300

    Create ErrorState component

---
+++ src/pages/error/components/ErrorState/ErrorState.jsx
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+import { IconWifiOff, IconRefresh } from "@tabler/icons-react";
+import styles from "./ErrorState.module.css";
+
+function ErrorState({ setRetryCount }) {
+  return (
+    <main className={styles["main"]}>
+      <div className={styles["error-icon-container"]}>
+        <IconWifiOff className={styles["error-icon"]} stroke={2} />
+      </div>
+      <p className={styles["error-title"]}>Couldn't load categories</p>
+      <p className={styles["error-msg"]}>
+        Something went wrong while fetching data.
+        <br />
+        Please try again.
+      </p>
+      <button
+        className={styles["retry-btn"]}
+        onClick={() => setRetryCount((c) => c + 1)}
+      >
+        <IconRefresh className={styles["retry-icon"]} stroke={2} />
+        <span className={styles["retry-btn-text"]}>Try again</span>
+      </button>
+    </main>
+  );
+}
+
+ErrorState.propTypes = {
+  setRetryCount: PropTypes.func,
+};
+
+export default ErrorState;
