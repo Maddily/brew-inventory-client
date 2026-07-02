a57e9577f2cfedf66b9a2a6fddc81bb109b8c84a
Maddily <115709272+Maddily@users.noreply.github.com>
2026-07-02 15:39:25 +0300

    Highlight the link when active

--- src/components/BottomNavLink/BottomNavLink.jsx
+++ src/components/BottomNavLink/BottomNavLink.jsx
@@ -1,11 +1,21 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router";
 import { Icon } from "@mdi/react";
+import { ActiveLinkContext } from "../../contexts";
 import styles from "./BottomNavLink.module.css";
+import { useContext } from "react";
 
-function BottomNavLink({ value, path, iconPath }) {
+function BottomNavLink({ value, path, iconPath, id }) {
+  const { active, setActive } = useContext(ActiveLinkContext);
+
   return (
-    <Link className={styles["bottom-nav-link"]} to={path}>
+    <Link
+      className={`${styles["bottom-nav-link"]} ${
+        active === id ? styles["active"] : ""
+      }`}
+      to={path}
+      onClick={() => id && setActive(id)}
+    >
       <Icon className={styles["icon"]} path={iconPath} size={0.7} />
       <span className={styles["text"]}>{value}</span>
     </Link>
@@ -16,6 +26,7 @@ BottomNavLink.propTypes = {
   value: PropTypes.string,
   path: PropTypes.string,
   iconPath: PropTypes.any,
+  id: PropTypes.number,
 };
 
 export default BottomNavLink;
