fd9ace4f95c16bd74d9564f84c523b94c83894fb
Maddily <115709272+Maddily@users.noreply.github.com>
2026-07-02 15:40:49 +0300

    Provide ActiveLinkContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
+import { ActiveLinkContext } from "./contexts";
 import NavBar from "./components/NavBar/NavBar";
 import BottomNav from "./components/BottomNav/BottomNav";
 import "./styles/normalize.css";
 import "./styles/App.css";
+import { Outlet } from "react-router";
 
 function App() {
+  const [active, setActive] = useState(0);
+
   return (
-    <>
-      <NavBar />
-      <BottomNav />
-    </>
+    <ActiveLinkContext value={{ active, setActive }}>
+      <NavBar active={active} setActive={setActive} />
+      <Outlet />
+      <BottomNav active={active} setActive={setActive} />
+    </ActiveLinkContext>
   );
 }
 
