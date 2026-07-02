8b89fc106229dc8b61e38a24eee3319caaf60f83
Maddily <115709272+Maddily@users.noreply.github.com>
2026-07-02 15:42:39 +0300

    Use ActiveLinkContext to highlight the active link

--- src/components/NavLink/NavLink.jsx
+++ src/components/NavLink/NavLink.jsx
@@ -1,10 +1,20 @@
 import { Link } from "react-router";
 import PropTypes from "prop-types";
 import styles from "./NavLink.module.css";
+import { ActiveLinkContext } from "../../contexts";
+import { useContext } from "react";
 
-function NavButton({ value, path }) {
+function NavButton({ value, path, id }) {
+  const { active, setActive } = useContext(ActiveLinkContext);
+
   return (
-    <Link className={styles["nav-btn"]} to={path}>
+    <Link
+      className={`${styles["nav-link"]} ${
+        active === id ? styles["active"] : ""
+      }`}
+      to={path}
+      onClick={() => id && setActive(id)}
+    >
       {value}
     </Link>
   );
@@ -13,6 +23,7 @@ function NavButton({ value, path }) {
 NavButton.propTypes = {
   value: PropTypes.string,
   path: PropTypes.string,
+  id: PropTypes.number,
 };
 
 export default NavButton;
