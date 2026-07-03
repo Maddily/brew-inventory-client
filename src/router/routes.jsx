c7be0d8518cc0c6e836e8cf7f2d4b3c5d74d0d87
Maddily <115709272+Maddily@users.noreply.github.com>
2026-07-03 12:00:53 +0300

    Add categories and products routes

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,9 +1,21 @@
 import App from "../App";
+import Categories from "../pages/categories/components/Categories/Categories";
+import Products from "../pages/products/components/Products/Products";
 
 const routes = [
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Categories />,
+      },
+      {
+        path: "/products",
+        element: <Products />,
+      },
+    ],
   },
 ];
 
