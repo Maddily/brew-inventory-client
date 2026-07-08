7fb3a7cb978d7c4e57d841b3e4f6e39e2c127d1e
Maddily <115709272+Maddily@users.noreply.github.com>
2026-07-08 21:32:15 +0300

    Create ProductRow component

---
+++ src/pages/products/components/ProductRow/ProductRow.jsx
@@ -0,0 +1,65 @@
+import PropTypes from "prop-types";
+import { useNavigate } from "react-router";
+import {
+  IconCircleCheck,
+  IconAlertTriangle,
+  IconCircleX,
+} from "@tabler/icons-react";
+import styles from "./ProductRow.module.css";
+
+function ProductRow({ name, price, stockQuantity, category, path }) {
+  const navigate = useNavigate();
+
+  return (
+    <tr className={styles["product-row"]} onClick={() => navigate(path)}>
+      <td className={styles["name-and-category"]}>
+        <p className={styles["name"]}>{name}</p>
+        <p className={styles["category"]}>{category}</p>
+      </td>
+      <td className={styles["price"]}>
+        {new Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+        }).format(price)}
+      </td>
+      <td className={styles["quantity"]}>{stockQuantity}</td>
+      <td
+        className={`${styles["availability"]} ${
+          stockQuantity > 10
+            ? styles["in-stock"]
+            : stockQuantity > 0
+            ? styles["low-stock"]
+            : styles["out-of-stock"]
+        }`}
+      >
+        <span>
+          {stockQuantity > 10 ? (
+            <IconCircleCheck className={styles["in-stock-icon"]} stroke={2} />
+          ) : stockQuantity > 0 ? (
+            <IconAlertTriangle
+              className={styles["low-stock-icon"]}
+              stroke={2}
+            />
+          ) : (
+            <IconCircleX className={styles["out-of-stock-icon"]} stroke={2} />
+          )}
+          {stockQuantity > 10
+            ? "In stock"
+            : stockQuantity > 0
+            ? "Low stock"
+            : "Out of stock"}
+        </span>
+      </td>
+    </tr>
+  );
+}
+
+ProductRow.propTypes = {
+  name: PropTypes.string,
+  price: PropTypes.string,
+  stockQuantity: PropTypes.string,
+  category: PropTypes.string,
+  path: PropTypes.string,
+};
+
+export default ProductRow;
