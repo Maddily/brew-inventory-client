48229827f3e3ed0f62bb9aca53e6179aaccc7ff5
Maddily <115709272+Maddily@users.noreply.github.com>
2026-07-08 21:26:05 +0300

    Create ProductsTable to render products in a table

---
+++ src/pages/products/components/ProductsTable/ProductsTable.jsx
@@ -0,0 +1,37 @@
+import styles from "./ProductsTable.module.css";
+import ProductRow from "../ProductRow/ProductRow";
+import PropTypes from "prop-types";
+
+function ProductsTable({ products, categoryId }) {
+  return (
+    <table className={styles["products-table"]}>
+      <thead>
+        <tr className={styles["table-head"]}>
+          <th>product</th>
+          <th>price</th>
+          <th>quantity</th>
+          <th>availability</th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map((product) => (
+          <ProductRow
+            key={product.id}
+            name={product.name}
+            price={product.price}
+            stockQuantity={product.stock_quantity}
+            category={!categoryId && product.category}
+            path={`/products/${product.id}`}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+ProductsTable.propTypes = {
+  products: PropTypes.array,
+  categoryId: PropTypes.string,
+};
+
+export default ProductsTable;
