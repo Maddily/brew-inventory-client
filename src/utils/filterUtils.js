ab4c7f38b4fe8d70613e3f96481786ba6de07336
Maddily <115709272+Maddily@users.noreply.github.com>
2026-07-11 13:08:39 +0300

    Create utility functions for filters

---
+++ src/utils/filterUtils.js
@@ -0,0 +1,39 @@
+export function filtersUnchanged(params, searchParams) {
+  const newCategoryIds = params.getAll("category_id").sort();
+  const currentCategoryIds = searchParams.getAll("category_id").sort();
+  const newAvailability = params.getAll("availability").sort();
+  const currentAvailability = searchParams.getAll("availability").sort();
+
+  return (
+    newCategoryIds.length === currentCategoryIds.length &&
+    newCategoryIds.every((id, i) => id === currentCategoryIds[i]) &&
+    newAvailability.length === currentAvailability.length &&
+    newAvailability.every((v, i) => v === currentAvailability[i])
+  );
+}
+
+export function populateSelectedChips(searchParams, setSelectedChips) {
+  const newSelectedChips = {
+    Category: [],
+    Availability: [],
+  };
+  const idToCategory = {
+    1: "Coffee",
+    2: "Tea",
+    3: "Ready-to-Drink",
+    4: "Accessories",
+  };
+
+  const categoryIds = searchParams.getAll("category_id");
+  for (const id of categoryIds) {
+    const category = idToCategory[id];
+    newSelectedChips.Category.push(category);
+  }
+
+  const availability = searchParams.getAll("availability");
+  for (const value of availability) {
+    newSelectedChips.Availability.push(value);
+  }
+
+  setSelectedChips(newSelectedChips);
+}
