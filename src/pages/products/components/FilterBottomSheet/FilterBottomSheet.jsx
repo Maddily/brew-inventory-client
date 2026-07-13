1d7128ff49efb1d0f65eb09f0943d6d65022c15c
Maddily <115709272+Maddily@users.noreply.github.com>
2026-07-13 23:40:41 +0300

    Add attribute filtering FilterBottomSheet
    - Add attribute sections based on current category
    (categoryIdToAttributes)
    - Remove Category section when viewing a single
    category's products
    - Move getAttributeValues to filterUtils
    - Extend selectedChips and applyFilters to handle
    attribute filter params
    - Add useParams to read current categoryId

--- src/pages/products/components/FilterBottomSheet/FilterBottomSheet.jsx
+++ src/pages/products/components/FilterBottomSheet/FilterBottomSheet.jsx
@@ -1,16 +1,16 @@
 import { Fragment, useContext } from "react";
-import { useSearchParams } from "react-router";
+import { useParams, useSearchParams } from "react-router";
 import { IconX } from "@tabler/icons-react";
 import PropTypes from "prop-types";
 import styles from "./FilterBottomSheet.module.css";
 import FilterSection from "../FilterSection/FilterSection";
 import { SelectedChipsContext } from "../../../../contexts";
-import { filtersChanged } from "../../../../utils/filterUtils";
+import {
+  filtersChanged,
+  getAttributeValues,
+} from "../../../../utils/filterUtils";
+import FilterEmptyState from "../FilterEmptyState/FilterEmptyState";
 
-const sections = {
-  Category: ["Coffee", "Tea", "Ready-to-Drink", "Accessories"],
-  Availability: ["In stock", "Low stock", "Out of stock"],
-};
 const categoryNameToId = {
   Coffee: 1,
   Tea: 2,
@@ -18,31 +18,78 @@ const categoryNameToId = {
   Accessories: 4,
 };
 
-function FilterBottomSheet({ filterBottomSheetRef, resetButtonStyle }) {
+const categoryIdToAttributes = {
+  1: ["Origin", "Roast Level", "Format", "Weight"],
+  2: ["Type", "Origin", "Format", "Caffeine Level", "Weight"],
+  3: ["Base", "Volume"],
+  4: ["Type", "Compatible With"],
+};
+
+function FilterBottomSheet({
+  filterBottomSheetRef,
+  resetButtonStyle,
+  products,
+}) {
   const [selectedChips, setSelectedChips] = useContext(SelectedChipsContext);
   const [searchParams, setSearchParams] = useSearchParams();
+  const { category_id: categoryId } = useParams();
+  const sections = {
+    Category: ["Coffee", "Tea", "Ready-to-Drink", "Accessories"],
+    Availability: ["In stock", "Low stock", "Out of stock"],
+  };
+
+  // Specific attribute sections are added based on the current list of products
+  // Category section is removed because the page displays products in one category
+  if (categoryId) {
+    delete sections.Category;
+
+    // Make a section for each attribute and add its values as chips
+    const attributes = categoryIdToAttributes[categoryId];
+    for (const attribute of attributes) {
+      sections[attribute] = getAttributeValues(products, attribute);
+    }
+  }
+
   const sectionKeys = Object.keys(sections);
 
   function clearFilters() {
     setSelectedChips({
       Category: [],
       Availability: [],
+      Origin: [],
+      "Roast Level": [],
+      Format: [],
+      Weight: [],
+      Type: [],
+      "Caffeine Level": [],
+      Base: [],
+      Volume: [],
+      "Compatible With": [],
     });
+    const params = new URLSearchParams();
+    setSearchParams(params);
+    filterBottomSheetRef.current.close();
   }
 
   function applyFilters() {
     const params = new URLSearchParams();
 
-    if (
-      selectedChips.Category.length > 0 ||
-      selectedChips.Availability.length > 0
-    ) {
-      selectedChips.Category.forEach((category) =>
-        params.append("category_id", categoryNameToId[category])
-      );
-      selectedChips.Availability.forEach((availability) =>
-        params.append("availability", availability)
-      );
+    for (const section in selectedChips) {
+      if (section === "Category") {
+        selectedChips[section].forEach((category) =>
+          params.append("category_id", categoryNameToId[category])
+        );
+        continue;
+      }
+
+      if (section === "Availability") {
+        selectedChips[section].forEach((a) => params.append("availability", a));
+        continue;
+      }
+
+      selectedChips[section].forEach((value) => {
+        params.append(section, value);
+      });
     }
 
     if (filtersChanged(params, searchParams)) {
@@ -103,6 +150,7 @@ FilterBottomSheet.propTypes = {
     current: PropTypes.instanceOf(Element),
   }),
   resetButtonStyle: PropTypes.func,
+  products: PropTypes.array,
 };
 
 export default FilterBottomSheet;
