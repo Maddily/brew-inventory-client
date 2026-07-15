59c403d4ef4b23af363bc193e75a5853d9651695
Maddily <115709272+Maddily@users.noreply.github.com>
2026-07-15 22:58:35 +0300

    Specify a different class name based on prop
    The section title class name changes based on
    the type of the filering window (bottom sheet
    vs dropdown)

--- src/pages/products/components/FilterSection/FilterSection.jsx
+++ src/pages/products/components/FilterSection/FilterSection.jsx
@@ -2,11 +2,13 @@ import PropTypes from "prop-types";
 import styles from "./FilterSection.module.css";
 import SheetChip from "../SheetChip/SheetChip";
 
-function FilterSection({ sections, section }) {
+function FilterSection({ sections, section, type }) {
   return (
     <>
       <div className={styles["sheet-section"]}>
-        <div className={styles["sheet-section-title"]}>{section}</div>
+        <div className={`${styles["sheet-section-title"]} ${styles[type]}`}>
+          {section}
+        </div>
         <div className={styles["sheet-chips"]}>
           {sections[section].map((value) => (
             <SheetChip key={value} section={section} value={value} />
@@ -20,6 +22,7 @@ function FilterSection({ sections, section }) {
 FilterSection.propTypes = {
   sections: PropTypes.objectOf(PropTypes.arrayOf(PropTypes.string)),
   section: PropTypes.string,
+  type: PropTypes.string,
 };
 
 export default FilterSection;
