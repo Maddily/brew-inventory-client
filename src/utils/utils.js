d5eac13ecb860c11695b539ca6ae95e732cfbdb1
Maddily <115709272+Maddily@users.noreply.github.com>
2026-07-21 19:19:58 +0300

    Create shouldUseSelect utility function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,12 @@
+import { categoryAttributes } from "../constants";
+
 export function formatPrice(price) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
   }).format(price);
+}
+
+export function shouldUseSelect(attr, category) {
+  return !!categoryAttributes[category]?.[attr];
 }
