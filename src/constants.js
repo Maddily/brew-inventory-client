e9dfd8acf83f5ea7d46724b6566c242c657e0ea0
Maddily <115709272+Maddily@users.noreply.github.com>
2026-07-21 19:23:08 +0300

    Add categoryAttributes
    It's a map of category to attributes and attribute
    to values, with a null value meaning the value
    can be anything and an array value meaning the
    value must be one of the specified values.

--- src/constants.js
+++ src/constants.js
@@ -18,3 +18,27 @@ export const categoryIdToClassName = {
   3: "rtd",
   4: "acc",
 };
+
+export const categoryAttributes = {
+  Coffee: {
+    Origin: null,
+    "Roast Level": ["Light", "Medium", "Dark", "Espresso"],
+    Format: ["Whole Bean", "Ground"],
+    Weight: null,
+  },
+  Tea: {
+    Type: ["Green", "Black", "White", "Oolong", "Herbal"],
+    Origin: null,
+    Format: ["Loose Leaf", "Bagged"],
+    "Caffeine Level": ["None", "Low", "Medium", "High"],
+    Weight: null,
+  },
+  "Ready-to-Drink": {
+    Base: ["Coffee", "Tea", "Other"],
+    Volume: null,
+  },
+  Accessories: {
+    Type: ["Filter", "Frother", "Infuser", "Grinder", "Scale", "Other"],
+    "Compatible With": ["Coffee", "Tea", "Both"],
+  },
+};
