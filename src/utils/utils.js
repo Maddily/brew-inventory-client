2beca0f55b7d8207905fc25a150d0966018a662d
Maddily <115709272+Maddily@users.noreply.github.com>
2026-07-26 21:22:46 +0300

    Add navigateBack functions

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -27,3 +27,23 @@ export const closeModalWithAnimation = (modal) => {
     easing: "ease-out",
   }).onfinish = () => modal.close();
 };
+
+export function navigateBackAfterEdit(id, categoryId, categoryName, navigate) {
+  navigate(`/products/${id}`, {
+    state: categoryId
+      ? {
+          from: "category",
+          categoryId,
+          categoryName,
+        }
+      : { from: "all" },
+  });
+}
+
+export function navigateBackAfterAdd(categoryId, navigate) {
+  if (categoryId) {
+    navigate(`/categories/${categoryId}`);
+  } else {
+    navigate("/products");
+  }
+}
