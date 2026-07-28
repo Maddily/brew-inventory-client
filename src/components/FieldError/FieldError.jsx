7559182fc0bf6f542dd8ffc50f568d85c330e50e
Maddily <115709272+Maddily@users.noreply.github.com>
2026-07-28 14:22:42 +0300

    Create FieldError component

---
+++ src/components/FieldError/FieldError.jsx
@@ -0,0 +1,20 @@
+import { IconAlertCircle } from "@tabler/icons-react";
+import styles from "./FieldError.module.css";
+import PropTypes from "prop-types";
+
+function FieldError({ message }) {
+  if (!message) return null;
+
+  return (
+    <div className={styles["field-error"]}>
+      <IconAlertCircle className={styles["field-error-icon"]} stroke={2} />
+      {message}
+    </div>
+  );
+}
+
+FieldError.propTypes = {
+  message: PropTypes.string,
+};
+
+export default FieldError;
