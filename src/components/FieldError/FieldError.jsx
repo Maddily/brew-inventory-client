678838a0f6851de254ec0ca6706f961e8ef19a5f
Maddily <115709272+Maddily@users.noreply.github.com>
2026-08-08 20:20:00 +0300

    Make FieldError more accessible
    - I used role="alert" to make sure the error is
    announced by screen readers upon occurring
    - I gave an id to the error and associated it
    with its corresponding input using aria-describedBy

--- src/components/FieldError/FieldError.jsx
+++ src/components/FieldError/FieldError.jsx
@@ -2,11 +2,11 @@ import { IconAlertCircle } from "@tabler/icons-react";
 import styles from "./FieldError.module.css";
 import PropTypes from "prop-types";
 
-function FieldError({ message }) {
+function FieldError({ message, id }) {
   if (!message) return null;
 
   return (
-    <div className={styles["field-error"]}>
+    <div id={id} className={styles["field-error"]} role="alert">
       <IconAlertCircle
         className={styles["field-error-icon"]}
         stroke={2}
