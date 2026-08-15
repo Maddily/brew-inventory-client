687da6df23d3b8eec43ee897c5d27f397ad848a4
Maddily <115709272+Maddily@users.noreply.github.com>
2026-08-15 21:48:52 +0300

    Memoize ProductsTable component
    This is to avoid unnecessary re-renders

--- src/pages/products/components/ProductsTable/ProductsTable.jsx
+++ src/pages/products/components/ProductsTable/ProductsTable.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styles from "./ProductsTable.module.css";
 import ProductRow from "../ProductRow/ProductRow";
 import PropTypes from "prop-types";
@@ -43,4 +44,4 @@ ProductsTable.propTypes = {
   categoryId: PropTypes.string,
 };
 
-export default ProductsTable;
+export default React.memo(ProductsTable);
